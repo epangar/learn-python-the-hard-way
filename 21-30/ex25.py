e3d1c4a79b9787a05bef89efedabe5f7787da8ec
Estanislao Pan García <epan@indra.es>
2020-01-03 16:40:42 +0100

    Exercise 24 & 25

---
+++ 21-30/ex25.py
@@ -0,0 +1,8 @@
+def break_words(stuff):
+    """This function will break up words for us. """
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sort the words."""
+    return sorted(words)
