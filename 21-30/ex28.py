d6887b48b68c8c53a671f621bdb565efea29b626
Estanislao Pan García <epan@indra.es>
2020-01-09 17:59:15 +0100

    Doing exercise 28

---
+++ 21-30/ex28.py
@@ -0,0 +1,20 @@
+print " 1: %r" % (True and True)  #True
+print " 2: %r" % (False and True) #
+print " 3: %r" % (1 == 1 and 2 == 1) #
+print " 4: %r" % ("test" == "test") #
+print " 5: %r" % (1 == 1 or 2 != 1) #
+print " 6: %r" % (True and 1 == 1) #
+print " 7: %r" % (False and 0 != 0) #
+print " 8: %r" % (True or 1 == 1) #
+print " 9: %r" % ("test" == "testing") #
+print "10: %r" % (1 != 0 and 2 == 1) #
+print "11: %r" % ("test" != "testing") #
+print "12: %r" % ("test" == 1) #
+print "13: %r" % (not (True and False)) #
+print "14: %r" % (not (1 == 1 and 0 != 1)) #
+print "15: %r" % (not (10 == 1 or 1000 == 1000)) #
+print "16: %r" % (not (1 != 10 or 3 == 4)) #
+print "17: %r" % (not ("testing" == "testing" and "Zed" == "Cool Guy")) #
+print "18: %r" % (1 == 1 and not ("testing" == 1 or 1 == 0)) #
+print "19: %r" % ("chunky" == "bacon" and not (3 == 4 or 3 == 3)) #
+print "20: %r" % (3 == 3 and not ("testing" == "testing" or "Python" == "Fun")) #
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-python-the-hard-way
+
+The 52 exercises in "Learn Python the Hard Way", in Python 2.7
