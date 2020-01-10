4f7ea47a7c9843aa5084f2750132a45ab7a7efca
Estanislao Pan García <epan@indra.es>
2020-01-10 16:17:14 +0100

    Exercises 28 and 29

--- 21-30/ex28.py
+++ 21-30/ex28.py
@@ -1,20 +1,20 @@
-print " 1: %r" % (True and True)  #True
-print " 2: %r" % (False and True) #
-print " 3: %r" % (1 == 1 and 2 == 1) #
-print " 4: %r" % ("test" == "test") #
-print " 5: %r" % (1 == 1 or 2 != 1) #
-print " 6: %r" % (True and 1 == 1) #
-print " 7: %r" % (False and 0 != 0) #
-print " 8: %r" % (True or 1 == 1) #
-print " 9: %r" % ("test" == "testing") #
-print "10: %r" % (1 != 0 and 2 == 1) #
-print "11: %r" % ("test" != "testing") #
-print "12: %r" % ("test" == 1) #
-print "13: %r" % (not (True and False)) #
-print "14: %r" % (not (1 == 1 and 0 != 1)) #
-print "15: %r" % (not (10 == 1 or 1000 == 1000)) #
-print "16: %r" % (not (1 != 10 or 3 == 4)) #
-print "17: %r" % (not ("testing" == "testing" and "Zed" == "Cool Guy")) #
-print "18: %r" % (1 == 1 and not ("testing" == 1 or 1 == 0)) #
-print "19: %r" % ("chunky" == "bacon" and not (3 == 4 or 3 == 3)) #
-print "20: %r" % (3 == 3 and not ("testing" == "testing" or "Python" == "Fun")) #
+print " 1: %r" % (True and True)  #True, because both are True
+print " 2: %r" % (False and True) #False, because not all of them are True
+print " 3: %r" % (1 == 1 and 2 == 1) #False, because not all of them are True
+print " 4: %r" % ("test" == "test") #True, beacuse both are True
+print " 5: %r" % (1 == 1 or 2 != 1) #True, because at least one is True
+print " 6: %r" % (True and 1 == 1) #True, beacuse both are True
+print " 7: %r" % (False and 0 != 0) #False, because not all of them are True
+print " 8: %r" % (True or 1 == 1) #True, because at least one is True
+print " 9: %r" % ("test" == "testing") #False
+print "10: %r" % (1 != 0 and 2 == 1) #False, because not all of them are True
+print "11: %r" % ("test" != "testing") #True
+print "12: %r" % ("test" == 1) #False
+print "13: %r" % (not (True and False)) #True, because it's !False (not all of them are true)
+print "14: %r" % (not (1 == 1 and 0 != 1)) #False
+print "15: %r" % (not (10 == 1 or 1000 == 1000)) #False, because it's !True (at least one is true) 
+print "16: %r" % (not (1 != 10 or 3 == 4)) #False, because it's !True (at least one is true) 
+print "17: %r" % (not ("testing" == "testing" and "Zed" == "Cool Guy")) #True
+print "18: %r" % (1 == 1 and not ("testing" == 1 or 1 == 0)) #True
+print "19: %r" % ("chunky" == "bacon" and not (3 == 4 or 3 == 3)) #False
+print "20: %r" % (3 == 3 and not ("testing" == "testing" or "Python" == "Fun")) #False
